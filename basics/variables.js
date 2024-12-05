4e6624235eb2be2442aed71b131ff11d334de72a
Raza Fazal <147403121+razasalaar@users.noreply.github.com>
2024-12-05 11:44:28 +0000

    change

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 13525
+let accountEmail = "razafazal19@gmail.com"
+var acoountPassword = "1234"
+accountCity = "Jaipur"
+
+// accountId = 2 not allowed to declared again const
+
+accountEmail = "raza@gmail.com"
+accountPassword = "314142"
+accountCity = "lahore"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
