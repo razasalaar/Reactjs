0ef39516f5c6e784666239ad244452b63340f088
Raza Fazal <147403121+razasalaar@users.noreply.github.com>
2025-01-06 07:14:53 +0000

    string overall

---
+++ basics/string.js
@@ -0,0 +1,14 @@
+const name  = "raza"
+const repoCount = 50
+//console.log(name +  repoCount ) ; //old
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const newString = "      raza   "
+console.log(newString.trim()) //remove extra space in data base
+
+const url = "hhtp://raza.com/raza%20salaar"
+console.log( url.replace('%20','-') ) // url in manners
+
+// string to array - string to array method
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
