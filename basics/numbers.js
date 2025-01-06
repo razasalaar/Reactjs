3ef6a97a32d26f8af307f7c5d1ef505a888d4767
Raza Fazal <147403121+razasalaar@users.noreply.github.com>
2025-01-06 07:40:33 +0000

    numbers and math

--- basics/numbers.js
+++ basics/numbers.js
@@ -4,3 +4,19 @@ const score = 400
 console.log(score)
 const balance = new Number(100)
 console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); //fixe value precision value
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(5) ); // precision value number utne anny jitne number hongy
+
+
+
+
+// maths
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6)); //max use
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
