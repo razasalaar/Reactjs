b5d81c3dccd3e9706f1849578d73416d982a524a
Raza Fazal <147403121+razasalaar@users.noreply.github.com>
2025-01-08 09:53:56 +0000

    date

---
+++ basics/datetime.js
@@ -0,0 +1,13 @@
+//Data 
+let myDate = new Date()
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleString());
+
+
+//create local date
+
+let myCreatedDate = new Date(2001,8,3)
+console.log(myCreatedDate.toDateString())
