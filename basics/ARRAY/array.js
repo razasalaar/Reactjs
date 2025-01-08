25bc67a12ecb51c927ced00c26a5b287d1cd050d
Raza Fazal <147403121+razasalaar@users.noreply.github.com>
2025-01-08 11:24:36 +0000

    array detailed

--- basics/ARRAY/array.js
+++ basics/ARRAY/array.js
@@ -1,7 +1,7 @@
 //array
 
-const myArr = [0, 1, 2, 3, 4, 5 ]
-const myHeroes = [ "raza", "salaar"]
+// const myArr = [0, 1, 2, 3, 4, 5 ]
+// const myHeroes = [ "raza", "salaar"]
 
 
 // const myArr2 = new Array(1, 2, 3, 4)
@@ -26,13 +26,36 @@ const myHeroes = [ "raza", "salaar"]
 
 //Slice or Splice difference
 
-console.log("A", myArr);
+// console.log("A", myArr);
 
-const myn1 = myArr.slice(1, 3)
-console.log(myn1)
-console.log("B" ,myArr)
+// const myn1 = myArr.slice(1, 3)
+// console.log(myn1)
+// console.log("B" ,myArr)
+
+// const myn2 = myArr.splice(1, 3)
+// console.log(myn2)
+// console.log("c" ,myArr)
 
 
-const myn2 = myArr.splice(1, 3)
-console.log(myn2)
-console.log("c" ,myArr)
+//PART 2
+
+const marvel_heroes = [ "thor", "ironman", "spiderman"]
+const dc_heroes = ["superman", "flashman", "batman"]
+
+// marvel_heroes.push(dc_heroes) //they can array in array array as data in this method
+// console.log(marvel_heroes);
+
+// const allhero = marvel_heroes.concat(dc_heroes) // two array merge method properly
+// console.log(allhero);
+
+//spread
+// const allnewhero = [...marvel_heroes, ...dc_heroes]  // this is other method to merge
+// console.log(allnewhero)
+
+// array ko array me ek array me krna
+// const another_array = [ 1,2,3,[4,5,6] ,7,[6,7,[4,5]]]
+
+// const real_another_array = another_array.flat(Infinity) 
+// console.log(real_another_array)
+console.log(Array.from("RAZA"))
+// arrayis from 
