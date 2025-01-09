b7516867f066b0ebfaae09f2f235a28e095e183a
Raza Fazal <147403121+razasalaar@users.noreply.github.com>
2025-01-09 15:49:34 +0000

    object detail

--- basics/ARRAY/Object/object.js
+++ basics/ARRAY/Object/object.js
@@ -36,5 +36,53 @@
 
 
 //singleton is litral ki trha declare krein singleton nai bnta constructor sy bnta haa
-const tinderUser = new Object{}
+// const tinderUser = new Object() 
+
+const tinderUser = {}
+
+tinderUser.id = "raza"
+tinderUser.name = "Salaar"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+// const  regularUser = {
+//     email: "razafazal@gmail.com",
+//     fullname: {
+//         userFullname: {
+//             firstName: "raza",
+//             lastName: "fazal"
+
+//         }
+        
+//     }
+// }
+// console.log(regularUser.fullname.userFullname.firstName);
+
+//add two object combine/ merge
+
+const obj1 = { 1:"a", 2:"b"}
+const obj2 = { 3:"c", 4:"d"}
+
+// const obj3 = Object.assign({},obj1,obj2) //merge use this in {}
+const obj3 = { ...obj1,...obj2} //latest and most used context
+// console.log(obj3);
+
+// array ka object
+const users = [
+    {
+        id: 1,
+        email: "razafazal19@gmail.com",
+
+    },
+    {
+        id: 2,
+        email: "salar23@gmail.com",
+
+    }
+]
+users[1].email
 console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
