306983a1134e42684dfda466ca31673e81323ea4
Raza Fazal <147403121+razasalaar@users.noreply.github.com>
2025-01-09 15:34:41 +0000

    object

--- basics/ARRAY/Object/object.js
+++ basics/ARRAY/Object/object.js
@@ -1,17 +1,17 @@
 //  OBJECT {}
-//singleton is litral ki trha declare krein singleton nai bnta constructor sy bnta haa
-//object literal
-const mySym = Symbol("key1")
-const JsUser = {
- name: "raza",
- age: 18,
- "fullname": "RAZA FAZAL",
- [mySym] : "mykey1", //declare symbol
- email: "razafazal313@gmail.com",
- isLoggedIn: false,
- lastLoginDays: ["Monday","Saturday"]
 
-}
+//object literal
+// const mySym = Symbol("key1")
+// const JsUser = {
+//  name: "raza",
+//  age: 18,
+//  "fullname": "RAZA FAZAL",
+//  [mySym] : "mykey1", //declare symbol
+//  email: "razafazal313@gmail.com",
+//  isLoggedIn: false,
+//  lastLoginDays: ["Monday","Saturday"]
+
+// }
 // console.log(JsUser.email) //1st method to declare object
 // console.log(JsUser["email"]) //right way to declare object
 // console.log( JsUser[mySym]) //right way to declare object
@@ -23,13 +23,18 @@ const JsUser = {
 // JsUser.email ="microsodf@gmail"
 // console.log(JsUser)
 
-JsUser.greeting = function () {
-    console.log("Hello Js user");
-}
+// JsUser.greeting = function () {
+//     console.log("Hello Js user");
+// }
 
-console.log(JsUser.greeting());
+// console.log(JsUser.greeting());
 
-JsUser.greetingTwo = function () {
-    console.log(`Hello Js user ${this.name}`); //how to refer a name in 
-}
-console.log(JsUser.greetingTwo());
+// JsUser.greetingTwo = function () {
+//     console.log(`Hello Js user ${this.name}`); //how to refer a name in 
+// }
+// console.log(JsUser.greetingTwo());
+
+
+//singleton is litral ki trha declare krein singleton nai bnta constructor sy bnta haa
+const tinderUser = new Object{}
+console.log(tinderUser);
