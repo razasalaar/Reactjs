23bf8f6647d654c4f03ac5719878ca3f31514a25
Raza Fazal <147403121+razasalaar@users.noreply.github.com>
2025-01-17 09:49:53 +0000

    funtion complete

--- Funtions/funtion.js
+++ Funtions/funtion.js
@@ -31,5 +31,28 @@ function loginUserMessage(username){
     return `${username} just logged in`
 
 }
-console.log(loginUserMessage("SALAAR"));
+// console.log(loginUserMessage("SALAAR"));
 
+// funtion with object and array// //*******************************/
+
+function calculateCartPrice(val1,val2,...num1) {
+    return num1
+}
+// console.log(calculateCartPrice(3,4,3,4,9));
+
+
+const user = {
+    username : "RAZA",
+    prices: 199
+}
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);  
+}
+handleObject(user)
+
+const myNewArray = [200,300,400,600]
+
+function returnSecondValue(getarray) {
+    return getarray[1]
+}
+console.log(returnSecondValue(myNewArray));
