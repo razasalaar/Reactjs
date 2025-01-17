014eabc0fa7935db212ae659fe67ef344acf19dd
Raza Fazal <147403121+razasalaar@users.noreply.github.com>
2025-01-17 10:05:42 +0000

    global and local scope

---
+++ Funtions/scope.js
@@ -0,0 +1,17 @@
+
+
+let a = 300; //global scope
+console.log(a);
+if (true) {
+    a =2
+    console.log("INNER",a);  
+}
+
+// console.log(b);
+// console.log(c);
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
